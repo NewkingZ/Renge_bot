b4867f2d1047be41cfa9bd8c154f7fca9edd4dcf
Antoine <Antoine@giatec.ca>
2020-10-03 17:16:07 -0400

    Added mostPlayed function

--- cogs/league.py
+++ cogs/league.py
@@ -8,12 +8,14 @@ import json
 
 LEAGUE_HEADER = "https://na1.api.riotgames.com/lol/"
 
-LEAGUE_COMMANDS = {"get_summoner_by_name": "summoner/v4/summoners/by-name/",
-                   "get_summoner_by_account": "summoner/v4/summoners/by-account/"}
+LEAGUE_COMMANDS = {"get_summoner": "summoner/v4/summoners/by-name/",
+                   "get_most_played": "champion-mastery/v4/champion-masteries/by-summoner/"}
 
 # Maximum # of commands for bot is 20 requests / second, 100 / 2 minutes
 load_dotenv()
 LEAGUE_KEY = os.getenv('RIOT_KEY')
+
+CHAMPIONS = None
 
 
 class League(commands.Cog):
@@ -40,7 +42,37 @@ class League(commands.Cog):
             content = json.loads(response.content.decode())
             await ctx.send(f'{user} is level {content["summonerLevel"]}. Encrypted ID: {content["id"]}')
 
+    @commands.command()
+    async def mostPlayed(self, ctx, *, user):
+        _url = LEAGUE_HEADER + LEAGUE_COMMANDS["get_summoner"] + user
+        headers = {
+            "Origin": "https://developer.riotgames.com",
+            "X-Riot-Token": LEAGUE_KEY
+        }
+        response = requests.get(_url, headers=headers)
+        if response.status_code != 200:
+            await ctx.send(f'Unsuccessful request referencing summoner ID. Code: {response.status_code}')
+            return
+
+        content = json.loads(response.content.decode())
+        _url = LEAGUE_HEADER + LEAGUE_COMMANDS["get_most_played"] + content["id"]
+        response = requests.get(_url, headers=headers)
+
+        if response.status_code != 200:
+            await ctx.send(f'Unsuccessful request getting mastery list. Code: {response.status_code}')
+            return
+
+        content = json.loads(response.content.decode())
+        results = f'Summoner {user}\'s most played are:\n'
+
+        for i in range(5):
+            results = results + f'\t{CHAMPIONS[str(content[i]["championId"])]} with {content[i]["championPoints"]}\n'
+        await ctx.send(results)
+
 
 def setup(client):
     client.add_cog(League(client))
+    global CHAMPIONS
+    with open('resources/champion.json') as f:
+        CHAMPIONS = json.load(f)
 
